a30b24f6d5ace711b249db7cd99842bde856dc56
Jeff Taylor <jefftaylor42@gmail.com>
2020-03-20 10:45:05 -0700

    Change the bucket names for our serratus terraform state.
    Ideally I would never have to do this, but I'm trying to migrate
    it across accounts.  And I can't figure out how to give the
    jefft.second account access to the bucket owned by jefftaylor42.
    
    I tried applying a grant block.  This allows operations such as
    "aws s3 cp" (write and read) access from jefft.second.  But
    "terraform init" returns an error:
    
        Error: Error importing AWS S3 bucket policy: AccessDenied: Access Denied
    
    It's not at all obvious what's happening, I don't have any more
    detailed logs, and there isn't anything useful stored in the bucket
    (I ran terraform destroy).  So, I'm recreating them from scratch.

--- terraform/terraform-state/main.tf
+++ terraform/terraform-state/main.tf
@@ -1,9 +1,17 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "serratus-terraform-state"
+  bucket = "serratus-tf-state"
+  region = "us-west-2"
 
   # Enable versioning so we can see history of the state files
   versioning {
     enabled = true
+  }
+
+  grant {
+    # jefft.second
+    type = "CanonicalUser"
+    id = "dcd86c2c4a99bfb6458a936948b17280a6120ebfa47491c638ae772477fefea0"
+    permissions = ["FULL_CONTROL"]
   }
 
   server_side_encryption_configuration {
@@ -16,7 +24,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "serratus-terraform-locks"
+  name         = "serratus-tf-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
