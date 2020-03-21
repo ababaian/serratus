47b205edaf78afb01a48c1f42e0a6bf0b12c5872
Jeff Taylor <jefftaylor42@gmail.com>
2020-03-20 17:10:47 -0700

    Add an error trap to serratus-dl.sh

--- scripts/serratus-dl/serratus-dl.sh
+++ scripts/serratus-dl/serratus-dl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -eux
 # ENTRYPOINT SCRIPT ===================
 # serrtaus-dl
 # =====================================
-set -ex
 #
 # Base: serratus-Downloader (>0.1.1)
 # Amazon Linux 2 with Docker
@@ -63,11 +63,6 @@ function usage {
   echo "ex: docker exec serratus-dl -u localhost:8000"
   exit 1
 }
-
-# PARSE INPUT =============================================
-# Generate random alpha-numeric for run-id
-RUNID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 )
-
 # Scheduler / Container Parameters
 SCHED='localhost:8000'
 WORKERS='1'
@@ -139,227 +134,139 @@ while getopts u:w:n:s:ak:D:P:U:d:oh FLAG; do
 done
 shift $((OPTIND-1))
 
+# Parse SRA Accession ID
+ACC_ID=$(echo $JOB_JSON | jq -r .acc_id)
+SRA=$(echo $JOB_JSON | jq -r .sra_run_info.Run)
+
+# Set up a error trap.  If something goes wrong unexpectedly, this will send
+# a message to the scheduler before exiting.
+function error {
+    curl -s -X POST "$SCHED/jobs/split/$ACC_ID?status=split_err"
+    exit 0 # Already told the calling script.
+}
+trap error ERR
+
 # Check inputs --------------
-if [[ ( -z "$OUTNAME" ) ]]; then OUTNAME="$SRA"; fi
+if [[ ( -z "$OUTNAME" ) ]];
+    then OUTNAME="$SRA"
+fi
 
-# FUNCTIONS ===============================================
-# Worker process.  Run in a loop, grabbing data from the scheduler and
-# processing it.
+# TODO: Allow the scheduler/main data-table to have arugments
+# which will be passed on to the downloader scripts
 
-function wait_for_scheduler {
-    while true; do
-        if [ "$(curl -s "$SCHED/status" | jq -r .status)" == "up" ]; then
-            break
-        else
-            sleep 5
-        fi
-    done
-}
+# TODO: If we're going to lock the system so the split commands
+# in each container don't collide on the CPU then we need to mount
+# a folder from filesystem into the container and `flock` a file
+# on the shared mount. For now use 1 worker per download EC2?
 
-function terminate_handler {
-    echo "    $ACC_ID was terminated without completing. Reset status."
-    echo "    In trap $(date -In)"
-    # Tell server to reset this job to a "new" state, since we couldn't
-    # finish processing it.
-    curl -s -X POST "$SCHED/jobs/split/$ACC_ID&status=new"
-}
+# Run job --------------------------------------------------
+# ----------------------------------------------------------
+# Generate random alpha-numeric for run-id
+RUNID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 )
+WORKDIR=$BASEDIR/$RUNID
+mkdir -p $WORKDIR; cd $WORKDIR
 
-function main_loop {
-    trap terminate_handler SIGUSR1
-    # Note: The "& wait"s are important.  Without them, bash will wait for
-    # the command to finish before executing its traps.  When we use "& wait",
-    # the command will recieve the same trap (killing it), and then run our
-    # trap handler, which tells the server our job failed.
-    # what if we & wait on the whole main_loop function instead? Test this.
-    WORKER_ID=$1
+echo "============================"
+echo "    serratus-dl Pipeline    "
+echo "============================"
+echo " date:      $(date)"
+echo " version:   $PIPE_VERSION"
+echo " ami:       $AMI_VERSION"
+echo " container: $CONTAINER_VERSION"
+echo " worker-id: $WORKER_ID"
+echo " run-id:    $RUNID"
+echo " sra:       $SRA"
+echo " S3 url:    $S3_BUCKET"
+echo ""
 
-    # TODO: Wrap job query into self-contained function?
-    while true; do
-        echo "$WORKERID - Requesting job from Scheduler..."
-        JOB_JSON=$(curl -s -X POST "$SCHED/jobs/split/")
-        ACTION=$(echo $JOB_JSON | jq -r .action)
+# RUN DOWNLOAD ==============================================
+echo "  Running -- run_download.sh --"
+echo "  $BASEDIR/run_download.sh -s $SRA $DL_ARGS"
 
-        case "$ACTION" in
-          process)
-            echo "  $WORKERID - Process State received."
-            ;;
-          wait)
-            echo "  $WORKERID - Wait State received."
-            sleep 3
-            continue
-            ;;
-          shutdown)
-            echo "  $WORKERID - Shutdown State received."
-            exit 0
-            ;;
-          *)        echo "  $WORKERID - ERROR: Unknown State received."
-            exit 1
-            echo "  $WORKERID - ERROR: Unknown State received."
-            exit 1
-        esac
+$BASEDIR/run_download.sh -s $SRA -n $THREADS $DL_ARGS
 
-        # Parse SRA Accession ID
-        ACC_ID=$(echo $JOB_JSON | jq -r .acc_id)
-        SRA_RUN=$(echo $JOB_JSON | jq -r .sra_run_info.Run)
+echo ''
 
-        # TODO: Allow the scheduler/main data-table to have arugments
-        # which will be passed on to the downloader scripts
-        
-        # TODO: If we're going to lock the system so the split commands
-        # in each container don't collide on the CPU then we need to mount
-        # a folder from filesystem into the container and `flock` a file
-        # on the shared mount. For now use 1 worker per download EC2?
+# Detect downloaded fastq files for split logic
+FQ0=$(ls *_0.fastq 2>/dev/null || true)
+FQ1=$(ls *_1.fastq 2>/dev/null || true)
+FQ2=$(ls *_2.fastq 2>/dev/null || true)
 
-        # Run job --------------------------------------------------
-        # ----------------------------------------------------------
-        # Generate random alpha-numeric for run-id
-        RUNID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 )
-        WORKDIR=$BASEDIR/$RUNID
-        mkdir -p $WORKDIR; cd $WORKDIR
+if [[ ( -s $FQ1 && -n $FQ1 ) && ( -s $FQ2 && -n $FQ2 ) ]]
+then
+  paired_exists=true
+  echo "  Paired-end reads detected"
+else
+  paired_exists=false
+  echo "  Paired-end reads not-detected"
+fi
 
-        echo "============================"
-        echo "    serratus-dl Pipeline    "
-        echo "============================"
-        echo " date:      $(date)"
-        echo " version:   $PIPE_VERSION"
-        echo " ami:       $AMI_VERSION"
-        echo " container: $CONTAINER_VERSION"
-        echo " worker-id: $WORKER_ID"
-        echo " run-id:    $RUNID"
-        echo " sra:       $SRA_RUN"
-        echo " S3 url:    $S3_BUCKET"
-        echo ""
+if [[ ( -s $FQ0 && -n $FQ0 ) ]]
+then
+  unpaired_exists=true
+  echo "  Unpaired reads detected"
+else
+  unpaired_exists=false
+  echo "  unpaired reads not-detected"
+fi
 
-        # RUN DOWNLOAD ==============================================
-        echo "  Running -- run_download.sh --"
-        echo "  $BASEDIR/run_download.sh -s $SRA_RUN $DL_ARGS"
+if [[ "$paired_exists" = true && "$unpaired_exists" = true ]]
+then
+  # Both paired-end and unpaired exist
+  # use only paired-end data
+  echo "   WARNING: Paired and Unpaired data detected"
+  echo "            Using only Paired-End Reads"
+  unpaired_exists=FALSE
+fi
 
-        $BASEDIR/run_download.sh -s $SRA_RUN -n $THREADS $DL_ARGS & wait
+# RUN SPLIT ===============================================
+# Add FQ0 vs. FQ1+FQ2 logic here
+echo "  Running -- run_split.sh --"
 
-        echo ''
+if [[ "$paired_exists" = true ]]
+then
+  echo "  .$BASEDIR/run_split.sh -o $OUTNAME -p $THREADS $SPLIT_ARGS"
+  bash $BASEDIR/run_split.sh -1 $FQ1 -2 $FQ2 -o $SRA -p $THREADS $SPLIT_ARGS
 
-        # Detect downloaded fastq files for split logic
-        FQ0=$(ls *_0.fastq 2>/dev/null || true)
-        FQ1=$(ls *_1.fastq 2>/dev/null || true)
-        FQ2=$(ls *_2.fastq 2>/dev/null || true)
+elif [[ "$unpaired_exists" = true ]]
+then
+  echo "  .$BASEDIR/run_split.sh -o $OUTNAME -p $THREADS $SPLIT_ARGS"
+  bash $BASEDIR/run_split.sh -f $FQ0 -o $SRA -p $THREADS $SPLIT_ARGS
 
-        if [[ ( -s $FQ1 && -n $FQ1 ) && ( -s $FQ2 && -n $FQ2 ) ]]
-        then
-          paired_exists=true
-          echo "  Paired-end reads detected"
-        else
-          paired_exists=false
-          echo "  Paired-end reads not-detected"
-        fi
+else
+  echo "   ERROR: Neither paired or unpaired reads detected"
+  # Update scheduler with fail message
+  exit 1
+fi
 
-        if [[ ( -s $FQ0 && -n $FQ0 ) ]]
-        then
-          unpaired_exists=true
-          echo "  Unpaired reads detected"
-        else
-          unpaired_exists=false
-          echo "  unpaired reads not-detected"
-        fi
+# Count output blocks
+N_paired=$( (ls *1.fq.* 2>/dev/null ) | wc -l)
+echo "    N paired-end fq-blocks: $N_paired"
 
-        if [[ "$paired_exists" = true && "$unpaired_exists" = true ]]
-        then
-          # Both paired-end and unpaired exist
-          # use only paired-end data
-          echo "   WARNING: Paired and Unpaired data detected"
-          echo "            Using only Paired-End Reads"
-          unpaired_exists=FALSE
-        fi
+N_unpaired=$((ls *0.fq.* 2>/dev/null ) | wc -l)
+echo "    N unpaired   fq-blocks: $N_unpaired"
+echo ""
 
-        # RUN SPLIT ===============================================
-        # Add FQ0 vs. FQ1+FQ2 logic here
-        echo "  Running -- run_split.sh --"
+# RUN UPLOAD ==============================================
+echo "  Running -- run_upload.sh --"
+echo "  $BASEDIR/run_upload.sh -k $S3_BUCKET -s $SRA $UL_ARGS"
 
-        if [[ "$paired_exists" = true ]]
-        then
-          echo "  .$BASEDIR/run_split.sh -o $OUTNAME -p $THREADS $SPLIT_ARGS"
-          bash $BASEDIR/run_split.sh -1 $FQ1 -2 $FQ2 -o $SRA_RUN -p $THREADS $SPLIT_ARGS & wait
+bash $BASEDIR/run_upload.sh \
+     -k $S3_BUCKET \
+     -s $SRA
 
-        elif [[ "$unpaired_exists" = true ]]
-        then
-          echo "  .$BASEDIR/run_split.sh -o $OUTNAME -p $THREADS $SPLIT_ARGS"
-          bash $BASEDIR/run_split.sh -f $FQ0 -o $SRA_RUN -p $THREADS $SPLIT_ARGS & wait
+echo "  Uploading complete."
+echo "  Status: DONE"
 
-        else
-          echo "   ERROR: Neither paired or unpaired reads detected"
-          # Update scheduler with fail message
-          exit 1
-        fi
+# CLEAN-UP ================================================
+# rm is super necessary since we're running in a loop. ;)
+cd $BASEDIR; rm -rf $WORKDIR/*
 
-        # Count output blocks
-        N_paired=$( (ls *1.fq.* 2>/dev/null ) | wc -l)
-        echo "    N paired-end fq-blocks: $N_paired"
-
-        N_unpaired=$((ls *0.fq.* 2>/dev/null ) | wc -l)
-        echo "    N unpaired   fq-blocks: $N_unpaired"
-        echo ""
-
-        # RUN UPLOAD ==============================================
-        echo "  Running -- run_upload.sh --"
-        echo "  $BASEDIR/run_upload.sh -k $S3_BUCKET -s $SRA_RUN $UL_ARGS"
-
-        bash $BASEDIR/run_upload.sh \
-             -k $S3_BUCKET \
-             -s $SRA_RUN & wait
-
-        echo "  Uploading complete."
-        echo "  Status: DONE"
-
-        # CLEAN-UP ================================================
-        # rm may be unneccesary since container is shutdown
-        cd $BASEDIR; rm -rf $WORKDIR/*
-
-        # Update to scheduler -------------------------------------
-        # ---------------------------------------------------------
-        ACC_ID=$(echo $JOB_JSON | jq -r .acc_id)
-        echo "  $WORKERID - Job $ACC_ID is complete. Update scheduler."
-        RESPONSE=$(curl -s -X POST "$SCHED/jobs/split/$ACC_ID?status=split_done&N_paired=$N_paired&N_unpaired=$N_unpaired")
-    done
-}
-
-
-# SCRIPT CORE ===================================
-echo "=========================================="
-echo "                SERRATUS                  "
-echo "=========================================="
-
-# Default base directory is /home/serratus
-cd $BASEDIR
-wait_for_scheduler
-
-# Fire up main loop (SRA downloader)
-for i in $(seq 1 "$WORKERS"); do
-    main_loop "$i" & worker[i]=$!
-done
-
-# Spot Operations ===============================
-# Monitor AWS Cloudwatch for spot-termination signal
-# if Spot termination signal detected, proceed with
-# shutdown via SIGURS1 signal
-
-# TODO: For a minor optimization; query the time of the
-# spot-termination signal and shutdown in the last 10
-# seconds to maximize chance the job finishes.
-
-METADATA=http://169.254.169.254/latest/meta-data
-while true; do
-    INSTANCE_ACTION=$(curl -fs $METADATA/spot/instance-action | jq -r .action)
-    if [ "$INSTANCE_ACTION" == "terminate" ]; then
-        echo "SPOT TERMINATION SIGNAL RECEIEVED."
-        echo "Initiating shutdown procedures for all workers"
-
-        for i in $(seq 1 "$WORKERS"); do
-            kill -USR1 ${worker[i]} 2>/dev/null || true
-        done
-        break
-    fi
-
-    sleep 60 # TODO Change this back to 5.
-done
+# Update to scheduler -------------------------------------
+# ---------------------------------------------------------
+ACC_ID=$(echo $JOB_JSON | jq -r .acc_id)
+echo "  $WORKERID - Job $ACC_ID is complete. Update scheduler."
+curl -s -X POST "$SCHED/jobs/split/$ACC_ID?status=split_done&N_paired=$N_paired&N_unpaired=$N_unpaired"
 
 echo "============================"
 echo "======= RUN COMPLETE ======="
---
+++ scripts/worker.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/bash
+set -eux
+# Default base directory is /home/serratus
+function wait_for_scheduler {
+    while true; do
+        if [ "$(curl -s "$SCHED/status" | jq -r .status)" == "up" ]; then
+            break
+        else
+            sleep 5
+        fi
+    done
+}
+
+function terminate_handler {
+    echo "    $ACC_ID was terminated without completing. Reset status."
+    echo "    In trap $(date -In)"
+    # Tell server to reset this job to a "new" state, since we couldn't
+    # finish processing it.
+    curl -s -X POST "$SCHED/jobs/split/$ACC_ID&status=new"
+}
+
+function main_loop {
+    trap terminate_handler SIGUSR1
+    # Note: The "& wait"s are important.  Without them, bash will wait for
+    # the command to finish before executing its traps.  When we use "& wait",
+    # the command will recieve the same trap (killing it), and then run our
+    # trap handler, which tells the server our job failed.
+    # what if we & wait on the whole main_loop function instead? Test this.
+    WORKERID=$1
+
+    # TODO: Wrap job query into self-contained function?
+    while true; do
+        echo "$WORKERID - Requesting job from Scheduler..."
+        JOB_JSON=$(curl -s -X POST "$SCHED/jobs/split/")
+        ACTION=$(echo $JOB_JSON | jq -r .action)
+
+        case "$ACTION" in
+          process)
+            echo "  $WORKERID - Process State received.  Running $@"
+            export JOB_JSON
+            "$@" & wait
+            ;;
+          wait)
+            echo "  $WORKERID - Wait State received."
+            sleep 3
+            continue
+            ;;
+          shutdown)
+            echo "  $WORKERID - Shutdown State received."
+            exit 0
+            ;;
+          *)        echo "  $WORKERID - ERROR: Unknown State received."
+            exit 1
+            echo "  $WORKERID - ERROR: Unknown State received."
+            exit 1
+        esac
+    done
+}
+
+echo "=========================================="
+echo "                SERRATUS                  "
+echo "=========================================="
+cd $BASEDIR
+wait_for_scheduler
+
+# Fire up main loop (SRA downloader)
+for i in $(seq 1 "$WORKERS"); do
+    main_loop "$i" & worker[i]=$!
+done
+
+# Spot Operations ===============================
+# Monitor AWS Cloudwatch for spot-termination signal
+# if Spot termination signal detected, proceed with
+# shutdown via SIGURS1 signal
+
+# TODO: For a minor optimization; query the time of the
+# spot-termination signal and shutdown in the last 10
+# seconds to maximize chance the job finishes.
+
+METADATA=http://169.254.169.254/latest/meta-data
+while true; do
+    # Note: this URL returns an HTML 404 page when there is no action.  Use
+    # "curl -f" to mitigate that.
+    INSTANCE_ACTION=$(curl -fs $METADATA/spot/instance-action | jq -r .action)
+    if [ "$INSTANCE_ACTION" == "terminate" ]; then
+        echo "SPOT TERMINATION SIGNAL RECEIEVED."
+        echo "Initiating shutdown procedures for all workers"
+
+        for i in $(seq 1 "$WORKERS"); do
+            kill -USR1 ${worker[i]} 2>/dev/null || true
+        done
+        break
+    fi
+
+    sleep 60 # TODO Change this back to 5.
+done
