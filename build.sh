4b5788af40520ee43cc4b2572ea0b0c70a969665
Jeff Taylor <jefftaylor42@gmail.com>
2020-03-29 15:26:25 -0700

    Add scheduler to build.sh, and push DOCKERHUB_USER to a variable.

--- build.sh
+++ build.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/bash
 set -eu
 # Build and push podman images in parallel.
-user=jefftaylor42
-images=${@:-dl align merge}
+images=${@:-dl align merge scheduler}
+DOCKERHUB_USER=${DOCKERHUB_USER:-}
+
+if [ -z "$DOCKERHUB_USER" ]; then
+    echo "Environment variable DOCKERHUB_USER not set."
+    exit 1
+fi
+
 podman build -f docker/Dockerfile -t serratus-base .
 for img in $images; do
     (
-        podman build -f "docker/serratus-$img/Dockerfile" -t $user/serratus-$img .
-        podman push -q $user/serratus-$img
+        if [ "$img" = scheduler ]; then
+            cd scheduler
+            podman build -t $DOCKERHUB_USER/serratus-$img .
+        else
+            podman build -f "docker/serratus-$img/Dockerfile" -t $DOCKERHUB_USER/serratus-$img .
+        fi
+
+        podman push -q $DOCKERHUB_USER/serratus-$img
         echo "Done pushing serratus-$img"
     ) &
 done
+
 wait
