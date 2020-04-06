778927f4effb23ce437bb4d488bd2c9a48ff77c7
Jeff Taylor <jefftaylor42@gmail.com>
2020-04-06 16:28:29 -0700

    Add grafana and prometheus to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -eu
 # Build and push podman images in parallel.
-images=${@:-dl align merge scheduler}
+images=${@:-dl align merge scheduler grafana prometheus}
 DOCKERHUB_USER=${DOCKERHUB_USER:-}
 
 if [ -z "$DOCKERHUB_USER" ]; then
@@ -13,8 +13,13 @@ podman build -f docker/Dockerfile -t serratus-base .
 for img in $images; do
     (
         if [ "$img" = scheduler ]; then
-            cd scheduler
+            pushd scheduler
             podman build -t $DOCKERHUB_USER/serratus-$img .
+            popd
+        elif [ "$img" = grafana -o "$img" = prometheus ]; then
+            pushd monitoring/"$img"
+            podman build -t $DOCKERHUB_USER/serratus-$img .
+            popd
         else
             podman build -f "docker/serratus-$img/Dockerfile" -t $DOCKERHUB_USER/serratus-$img .
         fi
