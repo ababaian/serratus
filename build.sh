a34e8b6be1d0126fec64b046fa146dcee29ad07e
EC2 Default User <ec2-user@ip-172-31-3-27.us-west-2.compute.internal>
2020-04-15 20:05:20 +0000

    ec2 build capability

--- build.sh
+++ build.sh
@@ -1,30 +1,56 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -eu
-# Build and push podman images in parallel.
+# Build and push container images in parallel.
 images=${@:-dl align merge scheduler grafana prometheus}
 DOCKERHUB_USER=${DOCKERHUB_USER:-}
+
+# Use `podman` or `docker` as builder
+#builder='podman'
+builder='sudo docker'
+
+# Container Version
+#VERSION=0 # Dev version
+VERSION=0.1.2
+
+# DOCKERHUB_USER='serratusbio'
+# sudo docker login
 
 if [ -z "$DOCKERHUB_USER" ]; then
     echo "Environment variable DOCKERHUB_USER not set."
     exit 1
 fi
 
-podman build -f docker/Dockerfile -t serratus-base .
+$builder build -f docker/Dockerfile \
+  -t serratus-base -t serratus-base:latest \
+  -t serratus-base:$VERSION .
+
 for img in $images; do
     (
         if [ "$img" = scheduler ]; then
             pushd scheduler
-            podman build -t $DOCKERHUB_USER/serratus-$img .
+            $builder build  \
+              -t serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img:$VERSION \
+              -t $DOCKERHUB_USER/serratus-$img:latest .
             popd
         elif [ "$img" = grafana -o "$img" = prometheus ]; then
             pushd monitoring/"$img"
-            podman build -t $DOCKERHUB_USER/serratus-$img .
+            $builder build  \
+              -t serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img:$VERSION \
+              -t $DOCKERHUB_USER/serratus-$img:latest .
             popd
         else
-            podman build -f "docker/serratus-$img/Dockerfile" -t $DOCKERHUB_USER/serratus-$img .
+            $builder build -f "docker/serratus-$img/Dockerfile" \
+              -t serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img \
+              -t $DOCKERHUB_USER/serratus-$img:$VERSION \
+              -t $DOCKERHUB_USER/serratus-$img:latest .
         fi
 
-        podman push -q $DOCKERHUB_USER/serratus-$img
+        $builder push $DOCKERHUB_USER/serratus-$img
         echo "Done pushing serratus-$img"
     ) &
 done
