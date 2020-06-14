47b426733f8625cfced4c1b6ffd2b9b6b66c9939
Jeff Taylor <jefftaylor42@gmail.com>
2020-06-13 18:09:23 -0700

    Metrics: go crazy.  Add postgres exporter, fix multiprocess metrics, update everything in grafana.

--- containers/serratus-scheduler/flask_app/metrics.py
+++ containers/serratus-scheduler/flask_app/metrics.py
@@ -1,51 +1,15 @@
 from flask import Blueprint, make_response
 import prometheus_client
-from prometheus_client import Gauge, multiprocess, REGISTRY
-from sqlalchemy import func
-
-from . import db
+from prometheus_client import multiprocess, CollectorRegistry
 
 bp = Blueprint("metrics", __name__, url_prefix="/metrics")
 
-# Multiprocess metric collection.
+REGISTRY = CollectorRegistry()
 multiprocess.MultiProcessCollector(REGISTRY)
-
-acc_state_gauge = Gauge(
-    "scheduler_accesion_state", "Number of accessions in each state", ["state"]
-)
-
-block_state_gauge = Gauge(
-    "scheduler_block_state", "Number of blocks in each state", ["state"]
-)
-
-
-def update_counts():
-    """Update counters for prometheus"""
-    session = db.get_session()
-
-    acc_bystate = (
-        session.query(db.Accession.state, func.count(db.Accession.state))
-        .group_by(db.Accession.state)
-        .all()
-    )
-    for state, count in acc_bystate:
-        acc_state_gauge.labels(state).set(count)
-
-    # Avoid "done" blocks, as this can involve counting a very large number
-    # of rows.
-    block_bystate = (
-        session.query(db.Block.state, func.count(db.Block.state))
-        .filter(db.Block.state != "done")
-        .group_by(db.Block.state)
-        .all()
-    )
-    for state, count in block_bystate:
-        block_state_gauge.labels(state).set(count)
 
 
 @bp.route("")
 def metrics():
-    update_counts()
-    r = make_response((prometheus_client.generate_latest(), 200))
+    r = make_response((prometheus_client.generate_latest(REGISTRY), 200))
     r.mimetype = "text/plain"
     return r
