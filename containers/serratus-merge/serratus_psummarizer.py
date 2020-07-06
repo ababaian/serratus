aa3769c51be341413aefb5d290b9adc9bbc64c5c
ababaian <ababaian@bccrc.ca>
2020-07-06 12:19:35 -0700

    pull updateds to summarizer from master

--- containers/serratus-merge/serratus_psummarizer.py
+++ containers/serratus-merge/serratus_psummarizer.py
@@ -3,39 +3,31 @@
 import sys
 import os
 
-SRA = os.getenv("SUMZER_SRA", "SRAxxxxxx")
+SRA = os.getenv("SUMZER_SRA", None)
 MAXALNS = int(os.getenv("SUMZER_MAXALNS", "10000000"))
 MAXX = int(os.getenv("SUMZER_MAXX", "100"))
 THROWX = (os.getenv("SUMZER_THROWX", "YES") != "NO")
-COMMENT = os.getenv("SUMZER_COMMENT", "")
+SUMZER_COMMENT = os.getenv("SUMZER_COMMENT", None)
 
 CVG_BINS = 25
 MINCVGPCT = 50
+
+ReportFileName = sys.argv[1]
+EchoFileName = None
+if len(sys.argv) > 2:
+	EchoFileName = sys.argv[2]
+
+fRep = open(ReportFileName, "w")
+fEcho = None
+if len(sys.argv) > 2:
+	fEcho = open(EchoFileName, "w")
+ExceptCount = 0
 
 '''
 #     ReadLabel               RefLabel  QLo     QHi      QL      TLo     THi      TL   PctId     Evalue
 #             0                      1    2       3       4        5       6       7       8          9
 ERR2756788.4545 Coronaviridae.repl1a.1  152     298     299     1230    1278    3990    57.1    1.1e-11
 ERR2756788.4927 Adenoviridae.Q9WF13.1   238     158     301     97      123     485     63.0    3.0e-06
-ERR2756788.5782 Coronaviridae.nucp.2    68      283     297     91      158     444     47.2    2.8e-12
-ERR2756788.6146 Poxviridae.Q6VZU9.1     132     16      301     76      114     325     53.8    6.5e-09
-ERR2756788.6146 Poxviridae.Q6VZU9.1     152     301     301     50      99      325     46.0    1.9e-08
-ERR2756788.6232 Coronaviridae.repl1a.1  156     1       302     2980    3031    4117    73.1    7.4e-21
-'''
-
-# Include bitscore, gaps and mismatches
-'''
-#        ReadLabel                   RefLabel  QLo     QHi       QL     TLo     THi      TL    PctId     Evalue BtSc  MiM     Gap
-#                0                          1    2       3        4       5       6       7        8          9   10   11      12
-ERR2756788.31345/1      Coronaviridae.R1AB.15   151     2       151     209     258     349     86.0    7.2e-22 96.3    7       0
-ERR2756788.31345/2      Coronaviridae.R1AB.15   9       149     151     181     227     349     80.9    3.7e-18 84.0    9       0
-ERR2756788.37873/1      Coronaviridae.R1AB.9    1       129     151     542     584     584     76.7    2.1e-13 68.2    10      0
-ERR2756788.43195/1      Coronaviridae.R1AB.15   151     2       151     67      116     349     70.0    7.0e-17 79.7    15      0
-ERR2756788.46979/1      Adenoviridae.CAPSH.1    129     1       151     1       43      498     48.8    1.6e-08 52.0    22      0
-ERR2756788.47790/2      Coronaviridae.R1AB.15   151     2       151     59      108     349     64.0    2.5e-14 71.2    18      0
-ERR2756788.49252/1      Coronaviridae.NCAP.2    148     11      151     75      120     337     45.7    4.8e-10 57.0    25      0
-ERR2756788.59541/1      Coronaviridae.SPIKE.3   149     3       151     167     215     651     75.5    8.2e-18 82.8    12      0
-ERR2756788.59541/2      Coronaviridae.SPIKE.3   89      148     149     162     181     651     85.0    2.3e-04 38.1    3       0
 '''
 
 def CmpKey__(i):
@@ -56,243 +48,126 @@ def GetOrder(Dict):
 	Order.reverse()
 	return Order
 
-fRep = open(sys.argv[1], "w")
-fEcho = None
-if len(sys.argv) > 2:
-	fEcho = open(sys.argv[2], "w")
-
-AlnCount = 0
-ExceptCount = 0
-
-FamilyToAlnCount = {}
-FamilyToPctIds = {}
-FamilyToGenes = {}
-FamilyToTotalAlnLength = {}
-
-FamilyGeneToLength = {}
-FamilyGeneToAlnCount = {}
-FamilyGeneToPctIds = {}
-FamilyGeneToBins = {}
-FamilyGeneToTotalAlnLength = {}
+def CountToSymbol(i):
+	#		    012345678901  2^11=2048
+	Symbols = "_.:uwaomUWAOM^"
+	n = 1
+	for c in Symbols:
+		if i < n:
+			return c
+		n *= 2
+	return "^"
 
 SumReadLength = 0
+AlnCount = 0
 
-def IncCount(D, k, n=1):
+IdToAlnCount = {}
+IdToSumPctId = {}
+IdToSumBasesPctId = {}
+IdToSumBases = {}
+IdToCoverageVec = {}
+
+Genes = []
+Families = []
+
+AccToLength = {}
+
+def IncDict(D, k, n=1):
 	try:
 		D[k] += n
 	except:
 		D[k] = n
 
-def AppendList(D, k, x):
-	try:
-		D[k].append(x)
-	except:
-		D[k] = [ x ]
+def IncCvg(Id, TLo, THi, TL):
+	global IdToCoverageVec
 
-def GetMedian(D, k):
 	try:
-		v = D[k]
+		v = IdToCoverageVec[Id]
 	except:
-		return -1
-	v.sort()
-	n = len(v)
-	Median = v[n/2]
-	return Median
+		IdToCoverageVec[Id] = [ 0 ]*CVG_BINS
 
-def IncCvg(FamilyGene, TLo, THi, TL):
-	try:
-		v = FamilyGeneToBins[FamilyGene]
-	except:
-		FamilyGeneToBins[FamilyGene] = [ 0 ]*CVG_BINS
-
-	Mid = (TLo + THi)/2
+	Mid = (TLo + THi)//2
 	Bin = ((Mid-1)*CVG_BINS)//TL
 	if Bin < 0:
 		Bin = 0
 	if Bin >= CVG_BINS:
 		Bin = CVG_BINS - 1
-	
-	FamilyGeneToBins[FamilyGene][Bin] += 1
+	IdToCoverageVec[Id][Bin] += 1
 
-def MakeCartoon(FamilyGene):
+def MakeCartoon(Id):
 	try:
-		w = FamilyGeneToBins[FamilyGene]
+		w = IdToCoverageVec[Id]
 	except:
-		return "?"
+		return "~"
 
-	Max = max(w)
-	if Max < 4:
-		Max = 4
 	s = ""
 	for i in w:
-		if i == 0:
-			s += '_'
-		elif i <= Max/4:
-			s += '.'
-		elif i <= Max/2:
-			s += 'o'
-		else:
-			s += 'O'
+		s += CountToSymbol(i)
 	return s 
 
-def GetCvgPct(FamilyGene):
+def GetDict(Dict, Key, Default = None):
 	try:
-		w = FamilyGeneToBins[FamilyGene]
+		return Dict[Key]
+	except:
+		return Default
+
+def GetCvgBins(Id, MinCount):
+	try:
+		v = IdToCoverageVec[Id]
 	except:
 		return 0
 
-	N = len(w)
+	N = len(v)
+	if N == 0:
+		return 0
 	n = 0
-	for i in w:
-		if i > 2:
+	for x in v:
+		if x >= MinCount:
 			n += 1
-	Pct = (n*100)//N
-	return Pct
-
-def AddFamilyGene(Family, Gene):
-	global FamilyToGenes
-	try:
-		v = FamilyToGenes[Family]
-	except:
-		FamilyToGenes[Family] = [ Gene ]
-		return
-
-	if not Gene in FamilyToGenes[Family]:
-		FamilyToGenes[Family].append(Gene)
-
-def GetGeneCount(Family):
-	try:
-		Genes = FamilyToGenes[Family]
-		return len(Genes)
-	except:
-		return 0
-
-def GetCoveredGenePcts(Family):
-	try:
-		Genes = FamilyToGenes[Family]
-	except:
-		return 0
-
-	Pcts = []
-	for Gene in Genes:
-		FamilyGene = Family + "." + Gene
-		CvgPct = GetCvgPct(FamilyGene)
-		if CvgPct >= MINCVGPCT:
-			Pcts.append(CvgPct)
-	return Pcts
-
-def GetCoveredGenesCount(Family):
-	Pcts = GetCoveredGenePcts(Family)
-	n = len(Pcts)
 	return n
 
-def GetAlnCount(Family):
-	try:
-		AlnCount = FamilyToAlnCount[Family]
-	except:
-		AlnCount = 0
-	return AlnCount
+def GetPctId(Id):
+	SumBases = GetDict(IdToSumBases, Id)
+	SumBasesPctId = GetDict(IdToSumBasesPctId, Id)
+	PctId = 0
+	if SumBases > 0:
+		PctId = float(SumBasesPctId)/SumBases
+	if PctId < 50:
+		PctId = 50
+	if PctId > 100:
+		PctId = 100
+	return PctId
 
-def GetTotalAlnLength(Family):
-	try:
-		n = FamilyToTotalAlnLength[Family]
-	except:
-		n = 0
-	return n
+def GetIdentityWeight(PctId):
+	FractId = PctId/100.0
+	if FractId < 0.5:
+		FractId = 0.5
+	if FractId > 1.0:
+		FractId = 1.0
+	Weight = 1.0/FractId
+	return Weight
 
-def GetScore(Family):
-	NA = GetAlnCount(Family)
-	Pcts = GetCoveredGenePcts(Family)
-	Pcts.sort()
-	N = len(Pcts)
-	TopPct = 0
-	SecondPct = 0
-	if N > 0:
-		TopPct = Pcts[N-1]
-	if N > 1:
-		SecondPct = Pcts[N-2]
-	Score = (TopPct + SecondPct)/4
-#	print >> sys.stderr, Family, Pcts, Score, NA
-	if NA > 5000:
-		Score += 50
-	elif NA > 2000:
-		Score += 40
-	elif NA > 1000:
-		Score += 20
-	elif NA > 100:
-		Score += 10
-	elif NA > 1:
-		Score += 5
-
+def GetScore(Id):
+	Cvg1 = GetCvgBins(Id, 1)
+	Cvg8 = GetCvgBins(Id, 8)
+	RawScore = Cvg8*4 + (Cvg1 - Cvg8)
+	PctId = GetPctId(Id)
+	Weight = GetIdentityWeight(PctId)
+	Score = RawScore*Weight
+	if Score > 100:
+		Score = 100
 	return Score
 
-def Rep(s):
-	global SRA
-	fRep.write(SRA + ";" + s + "\n")
+def GetGene(Label):
+	Fields = Label.split('.')
+	if len(Fields) == 1:
+		return Label
+	Gene = Fields[0] + "." + Fields[1]
+	return Gene
 
-def Report():
-	global AlnCount
-
-	s = "comment:%s;" % (COMMENT)
-	Rep(s)
-
-	MeanReadLength = 0
-	if AlnCount > 0:
-		MeanReadLength = SumReadLength/AlnCount
-	s = "totals:alns=%d;readlength=%d;" % (AlnCount, MeanReadLength)
-	if ExceptCount > 0:
-		s += "excepts=%d;" % ExceptCount
-	if AlnCount == MAXALNS:
-		s += "truncated=yes;"
-	else:
-		s += "truncated=no;"
-	Rep(s)
-
-	Order = GetOrder(FamilyToAlnCount)
-	Families = list(FamilyToAlnCount.keys())
-	n = len(Families)
-	for k in Order:
-		Family = Families[k]
-		AlnCount = FamilyToAlnCount[Family]
-		TotalAlnLength = FamilyToTotalAlnLength[Family]
-		AvgAlnlength = 0.0
-		if AlnCount > 0:
-			AvgAlnlength = float(TotalAlnLength)/AlnCount
-		MedPctId = GetMedian(FamilyToPctIds, Family)
-		GeneCount = GetGeneCount(Family)
-		CoveredGeneCount = GetCoveredGenesCount(Family)
-		Score = GetScore(Family)
-
-		s = "family:%s;" % Family
-		s += "score=%.0f;" % Score
-		s += "alns=%d;"% AlnCount
-		s += "avgcols=%.0f;" % AvgAlnlength
-		s += "pctid=%.1f;" % MedPctId
-		s += "genes=%d/%d;" % (CoveredGeneCount, GeneCount)
-		Rep(s)
-
-	Order = GetOrder(FamilyGeneToAlnCount)
-	FGs = list(FamilyGeneToAlnCount.keys())
-	n = len(FGs)
-	for k in Order:
-		FamilyGene = FGs[k]
-		AlnCount = FamilyGeneToAlnCount[FamilyGene]
-		MedPctId = GetMedian(FamilyGeneToPctIds, FamilyGene)
-		Length = FamilyGeneToLength[FamilyGene]
-		CvgPct = GetCvgPct(FamilyGene)
-		Cartoon = MakeCartoon(FamilyGene)
-		TotalAlnLength = FamilyGeneToTotalAlnLength[FamilyGene]
-		AvgAlnlength = 0.0
-		if AlnCount > 0:
-			AvgAlnlength = float(TotalAlnLength)/AlnCount
-		s = "gene:%s;" % FamilyGene
-		s += "alns=%d;"% AlnCount
-		s += "avgcols=%.0f;" % AvgAlnlength
-		s += "medpctid=%.1f;" % MedPctId
-		s += "len=%d;" % Length
-		s += "cvgpct=%d;" % CvgPct
-		s += "cvg=%s;" % Cartoon
-		Rep(s)
+def GetFamily(Label):
+	Family = Label.split('.')[0]
+	return Family
 
 def DoAln_():
 	global SumReadLength
@@ -303,24 +178,25 @@ def DoAln_():
 		assert False
 
 	RefLabel = Fields[1]
-	QLo = int(Fields[2])
-	QHi = int(Fields[3])
+#	QLo = int(Fields[2])
+#	QHi = int(Fields[3])
 	QL = int(Fields[4])
 	TLo = int(Fields[5])
 	THi = int(Fields[6])
 	TL = int(Fields[7])
 	PctId = float(Fields[8])
-	# Evalue = float(Fields[9])
-	# BtSc = float(Fields[10])
-	# MiM = int(Fields[11])
-	# Gap = int(Fields[12])
 
-	Fields = RefLabel.split('.')
-	assert len(Fields) == 3
-	Family = Fields[0]
-	Gene = Fields[1]
+	Acc = RefLabel
+	Family = GetFamily(RefLabel)
+	Gene = GetGene(RefLabel)
+	if Gene not in Genes:
+		Genes.append(Gene)
+	if Family not in Families:
+		Families.append(Family)
 
-	FamilyGene = Family + "." + Gene
+#	n = RefLabel.find('.')
+#	assert n > 0
+#	Family = RefLabel[:n]
 
 	SumReadLength += QL
 
@@ -329,15 +205,15 @@ def DoAln_():
 	else:
 		AlnLength = TLo - THi + 1
 
-	FamilyGeneToLength[FamilyGene] = TL
-	AddFamilyGene(Family, Gene)
-	IncCount(FamilyToAlnCount, Family)
-	IncCount(FamilyGeneToAlnCount, FamilyGene)
-	IncCount(FamilyToTotalAlnLength, Family, AlnLength)
-	IncCount(FamilyGeneToTotalAlnLength, FamilyGene, AlnLength)
-	AppendList(FamilyGeneToPctIds, FamilyGene, PctId)
-	AppendList(FamilyToPctIds, Family, PctId)
-	IncCvg(FamilyGene, TLo, THi, TL)
+	AccToLength[Acc] = TL
+#	AddFamilyAcc(Family, Acc)
+
+	for Id in [ Acc, Gene, Family ]:
+		IncCvg(Id, TLo, THi, TL)
+		IncDict(IdToAlnCount, Id)
+		IncDict(IdToSumBases, Id, AlnLength)
+		IncDict(IdToSumPctId, Id, PctId)
+		IncDict(IdToSumBasesPctId, Id, PctId*AlnLength)
 
 def DoAln():
 	global ExceptCount
@@ -351,6 +227,93 @@ def DoAln():
 		ExceptCount += 1
 		return
 
+def Rep(s):
+	global SRA
+	if SRA != None:
+		fRep.write("sra=" + SRA + ";")
+	fRep.write(s + "\n")
+
+def GetLine(Id):
+	global Genes
+
+	Cartoon = MakeCartoon(Id)
+	PctId = GetPctId(Id)
+	Score = GetScore(Id)
+	Alns = GetDict(IdToAlnCount, Id)
+	SumBases = GetDict(IdToSumBases, Id)
+	IsGene = (Id in Genes)
+	IsFamily = (Id in Families)
+
+	AvgCols = 0
+	if Alns > 0:
+		AvgCols = float(SumBases)/Alns
+
+	s = ""
+	if IsGene:
+		s += "gencvg=%s;" % Cartoon
+		s += "gen=%s;" % Id
+	elif IsFamily:
+		s += "famcvg=%s;" % Cartoon
+		s += "fam=%s;" % Id
+	else:
+		s += "seqcvg=%s;" % Cartoon
+		s += "seq=%s;" % Id
+	s += "score=%.0f;" % Score
+	s += "pctid=%.0f;" % PctId
+	s += "alns=%.0f;" % Alns
+	s += "avgcols=%.0f;" % AvgCols
+	return s
+
+def Report():
+	global AlnCount
+	global IdToScore
+
+	MeanReadLength = 0
+	if AlnCount > 0:
+		MeanReadLength = SumReadLength/AlnCount
+
+	s = ""
+	if SUMZER_COMMENT != None:
+		s += "SUMZER_COMMENT=" + SUMZER_COMMENT + ";"
+	s += "totalalns=%d;readlength=%d;" % (AlnCount, MeanReadLength)
+	if ExceptCount > 0:
+		s += "excepts=%d;" % ExceptCount
+	if AlnCount == MAXALNS:
+		s += "truncated=yes;"
+	else:
+		s += "truncated=no;"
+	Rep(s)
+
+	Ids = list(IdToAlnCount.keys())
+
+	IdToScore = {}
+	for Id in Ids:
+		Score = GetScore(Id)
+		IdToScore[Id] = Score
+	Order = GetOrder(IdToScore)
+	Ids = list(IdToScore.keys())
+
+	for k in Order:
+		Id = Ids[k]
+		if not Id in Families:
+			continue
+		s = GetLine(Id)
+		Rep(s)
+
+	for k in Order:
+		Id = Ids[k]
+		if not Id in Genes:
+			continue
+		s = GetLine(Id)
+		Rep(s)
+
+	for k in Order:
+		Id = Ids[k]
+		if Id in Genes or Id in Families:
+			continue
+		s = GetLine(Id)
+		Rep(s)
+
 for Line in sys.stdin:
 	if fEcho != None:
 		fEcho.write(Line)
@@ -359,4 +322,5 @@ for Line in sys.stdin:
 	if AlnCount >= MAXALNS or ExceptCount >= MAXX:
 		Report()
 		sys.exit(0)
+
 Report()
