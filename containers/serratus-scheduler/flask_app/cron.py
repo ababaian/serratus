6deb1145f3c4af0e6f0b9032f2e0c5a2f5156338
ababaian <ababaian@bccrc.ca>
2021-08-11 17:14:24 +0100

    revert verbose, run termination only

--- containers/serratus-scheduler/flask_app/cron.py
+++ containers/serratus-scheduler/flask_app/cron.py
@@ -12,8 +12,13 @@ from flask import current_app
 from flask.cli import with_appcontext
 from prometheus_client import Gauge, start_http_server, CollectorRegistry
 from sqlalchemy import or_
+import logging
 
 from . import db
+
+# Create application logger
+logger = logging.getLogger('workflow')
+logger.setLevel(logging.DEBUG)
 
 CRON_REGISTRY = CollectorRegistry()
 
@@ -27,6 +32,7 @@ asg_virt_size_gauge = Gauge(
 
 
 def get_asg_name(autoscaling, pattern):
+    print( 'Get Autosclaing via pattern: ', pattern )
     """Look through all available ASGs to find one matching a pattern"""
     paginator = autoscaling.get_paginator("describe_auto_scaling_groups").paginate()
     for page in paginator:
@@ -57,6 +63,9 @@ def set_asg_size(
     asg_virt_size_gauge.labels(name).set(asg_desired_size)
 
     asg_name = get_asg_name(autoscaling, "serratus-" + name)
+
+    print( '    -- asg scaling:', num_jobs,' jobs to ', asg_desired_size,' nodes')
+    
     try:
         autoscaling.set_desired_capacity(
             AutoScalingGroupName=asg_name, DesiredCapacity=asg_desired_size,
@@ -72,12 +81,13 @@ def set_asg_size(
 
 
 def adjust_autoscaling_loop(app):
-    time.sleep(10)  # Give postgres a few seconds to start
+    print('    -- asg loop')
+    print('       region: ', app.config["AWS_REGION"])
     autoscaling = boto3.session.Session().client(
         "autoscaling", region_name=app.config["AWS_REGION"]
     )
-
     while True:
+        print('       asg start')
         with app.app_context():
             config = dict(db.get_config())
 
@@ -89,13 +99,11 @@ def adjust_autoscaling_loop(app):
                 )
                 .count()
             )
-
             num_align_jobs = (
                 session.query(db.Block)
                 .filter(or_(db.Block.state == "new", db.Block.state == "aligning"))
                 .count()
             )
-
             exclude_accs = (
                 session.query(db.Block.acc_id)
                 .distinct()
@@ -108,8 +116,8 @@ def adjust_autoscaling_loop(app):
                 .filter(~(db.Accession.acc_id.in_(exclude_accs)))
                 .count()
             )
-
         if config["DL_SCALING_ENABLE"]:
+            print( '    -- asg: dl-scaling')
             constant = float(config["DL_SCALING_CONSTANT"])
             max_ = int(config["DL_SCALING_MAX"])
             virt_dl = set_asg_size(
@@ -121,6 +129,7 @@ def adjust_autoscaling_loop(app):
                 int(config["DL_MAX_INCREASE"]),
             )
         if config["ALIGN_SCALING_ENABLE"]:
+            print( '    -- asg: align-scaling')
             constant = float(config["ALIGN_SCALING_CONSTANT"])
             max_ = int(config["ALIGN_SCALING_MAX"])
             virt_align = set_asg_size(
@@ -132,6 +141,7 @@ def adjust_autoscaling_loop(app):
                 int(config["ALIGN_MAX_INCREASE"]),
             )
         if config["MERGE_SCALING_ENABLE"]:
+            print( '    -- asg: merge-scaling')
             constant = float(config["MERGE_SCALING_CONSTANT"])
             max_ = int(config["MERGE_SCALING_MAX"])
             virt_merge = set_asg_size(
@@ -142,7 +152,6 @@ def adjust_autoscaling_loop(app):
                 "merge",
                 int(config["MERGE_MAX_INCREASE"]),
             )
-
         scale_interval = int(config["SCALING_INTERVAL"])
         virt_interval = int(config["VIRTUAL_SCALING_INTERVAL"])
         if virt_dl or virt_align or virt_merge:
@@ -224,15 +233,20 @@ def clear_terminated_jobs():
     This should run inside of a session context, since the current DB doesn't
     handle transactions well.  What we should do is implement a global DB lock
     but I would need to test how that impacts performance."""
+    print( '       clearing terminated jobs')
+    
     instances = set(get_running_instances())
 
     check_and_clear(instances, db.Accession, "splitting", "new", "dl")
     check_and_clear(instances, db.Accession, "merging", "split_done", "merge")
     check_and_clear(instances, db.Block, "aligning", "new", "align")
 
+    print( '       clearing complete')
+
+
 
 def clean_terminated_jobs_loop(app):
-    time.sleep(10)  # Give postgres a few seconds to start
+    print('    -- termination loop')
     while True:
         with app.app_context():
             clear_interval = int(db.get_config_val("CLEAR_INTERVAL"))
@@ -243,12 +257,29 @@ def clean_terminated_jobs_loop(app):
 
 @click.command("cron")
 @with_appcontext
+
 def cron():
     print("Creating background processes")
+    print( '  verbose logging enabled')30
+
+    # Delay to allow postgres to boot
+    print( '  initializing...')
+    time.sleep(30)
+    print( '  initializing...')
+    time.sleep(30)
+    print( '  initializing...')
+    time.sleep(30)
+    print( '  initializing...')
+    time.sleep(30)
+
     app = current_app._get_current_object()
     start_http_server(9101, registry=CRON_REGISTRY)
+
+    #print('  starting autoscaling loop')
+    #Thread(target=adjust_autoscaling_loop, args=(app,)).start()
+
+    print('  starting termination loop')
     Thread(target=clean_terminated_jobs_loop, args=(app,)).start()
-    Thread(target=adjust_autoscaling_loop, args=(app,)).start()
 
 
 def register(app):
