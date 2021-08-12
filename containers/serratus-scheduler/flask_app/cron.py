169ab8881b11ced991bf27f5a093df277772b413
ababaian <ababaian@bccrc.ca>
2021-08-12 15:53:05 +0100

    show exception

--- containers/serratus-scheduler/flask_app/cron.py
+++ containers/serratus-scheduler/flask_app/cron.py
@@ -248,13 +248,22 @@ def clear_terminated_jobs():
 def clean_terminated_jobs_loop(app):
     print('    -- termination loop')
     while True:
-        with app.app_context():
+        try:
+            app.app_context()
             print('        db-get')
             clear_interval = int(db.get_config_val("CLEAR_INTERVAL"))
             print('        clear interval:', clear_interval)
-            clear_terminated_jobs()
-
-        time.sleep(clear_interval)
+            clear_terminated_jobs()    
+        except Exception as e:
+             print(e)
+             print(clear_interval)
+        # with app.app_context():
+        #     print('        db-get')
+        #     clear_interval = int(db.get_config_val("CLEAR_INTERVAL"))
+        #     print('        clear interval:', clear_interval)
+        #     clear_terminated_jobs()
+        print('   -- end loop')
+        time.sleep(30)
 
 
 @click.command("cron")
